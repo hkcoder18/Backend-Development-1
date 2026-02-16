3305ec625563cedad8f62b02f57f48a39263d233
hkcoder18 <harshkharabe007@gmail.com>
2026-02-16 21:03:12 +0530

    Database day-3

---
+++ Database/day-3.py
@@ -0,0 +1,10 @@
+# Constraints = Rules & restictions
+# we can apply constraints to the column
+
+# Main constraints in postresql.
+# 1. primary key = special identifier of a particular row (not null + unique)
+# 2. foregin key = these is used to define relation between 2 tables
+# 3. unique = each value in column should be unique
+# 4. not null = the column value must not be null
+# 5. default = it assign default value to the column
+# 6. check = condition
